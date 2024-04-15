868a6c32bfdaab61eefe810e605306ee8262ecde
Maya Ha <mayaha@cs.ucsb.edu>
2024-04-15 13:30:15 -0700

    bios

--- src/intervention_gptj_bios_profession.py
+++ src/intervention_gptj_bios_profession.py
@@ -118,6 +118,10 @@ class GPTJExperiment:
                 top_5_acc = float(answer.lower().strip() in [token.lower().strip() for token in top_k_tokens[:5]])
                 top_10_acc = float(answer.lower().strip() in [token.lower().strip() for token in top_k_tokens[:10]])
 
+                tokens_str = ",".join([token.lower().strip() for token in top_k_tokens[:5]])
+                with open("bios_results.txt", "a") as file1:
+                    file1.write(f"{int(is_correct == True)}\t{question}\t{answer}\t{tokens_str}\n")
+
                 # Compute log-prob of question and answer
                 selected_log_prob = log_prob[:-1, :]  # question - 1 x vocab
                 indices = inputs.input_ids[0, 1:].unsqueeze(1)  # question - 1 x 1
