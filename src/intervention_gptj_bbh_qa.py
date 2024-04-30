f736ecf90e06d7665d32935c0470cc37f592d6c6
Maya Ha <mayaha@cs.ucsb.edu>
2024-04-29 17:23:30 -0700

    prepared

--- src/intervention_gptj_bbh_qa.py
+++ src/intervention_gptj_bbh_qa.py
@@ -123,11 +123,7 @@ class GPTJExperiment:
 
             prepared_generation, prepared_answer = self.metrics._prepare(generation = generation, answer=answer)
             with open("bbh_qa_results.txt", "a") as file1:
-                    file1.write(f"{int(is_correct == True)}")
-                    file1.write(f"{generation}")
-                    file1.write(f"{answer}")
-                    file1.write(f"{prepared_generation}")
-                    file1.write(f"{prompt.encode('utf-8')}")
+                    file1.write(f"{int(is_correct == True)}\t{prepared_answer}\t{prepared_generation}\t{prompt}\n")
 
             self.dataset_metric.accept(is_correct=is_correct,
                                        f1pr_score=f1pr_score,
