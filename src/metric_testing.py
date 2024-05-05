74d0778db1dff620db99561431b0db9a9347a224
Maya Ha <mayaha@cs.ucsb.edu>
2024-05-05 13:54:52 -0700

    remove file if it exists

--- src/metric_testing.py
+++ src/metric_testing.py
@@ -1,6 +1,6 @@
 import subprocess
 import sys
-
+import os
 params = {"counterfact": ["fc_in", "27", "9.9"], 
           "hotpot": ["fc_in", "27", "9"], 
           "fever": ["fc_in", "24", "9.9"], 
@@ -16,6 +16,9 @@ file3_params = params[sys.argv[3]]
 intervention_file1 = f"intervention_gptj_{sys.argv[1]}.py"
 intervention_file2 = f"intervention_gptj_{sys.argv[2]}.py"
 intervention_file3 = f"intervention_gptj_{sys.argv[3]}.py"
+
+if os.path.exists('metric_comparison.txt'):
+    os.remove('metric_comparison.txt')
 
 subprocess.run(['python3', intervention_file1, '--lname', file1_params[0], '--lnum', file1_params[1], '--rate', file1_params[2]])
 subprocess.run(['python3', intervention_file2, '--lname', file1_params[0], '--lnum', file1_params[1], '--rate', file1_params[2]])
